08316794f43d0bf513e98caf69562143a8c46e4e
unknown <rouchashoori@gmail.com>
2024-08-02 16:04:22 +0330

    added a custom user manager for creating admin user without problem

--- register/models.py
+++ register/models.py
@@ -1,7 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 from django.contrib.auth.models import AbstractUser
-from datetime import timedelta, datetime
+from datetime import timedelta
 from django.utils import timezone
+
+
+# created a user manager for creating users without problem of custom username fields
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, **extra_fields):
+        # if phone number field not provided 
+        if not phone_number:
+            raise ValueError('The Phone Number is required')
+
+        # an instance of user model
+        user = self.model(phone_number=phone_number, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, phone_number, password=None, **extra_fields):
+        # making some admin fields to be their default values
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, **extra_fields)
+
 
 class User(AbstractUser):
     username     = None
@@ -11,6 +40,9 @@ class User(AbstractUser):
     email        = models.EmailField(unique=True, blank=True, null=True)
 
     USERNAME_FIELD = "phone_number"
+    REQUIRED_FIELDS = []
+    
+    objects = UserManager()
 
 
     def __str__(self) -> str:
