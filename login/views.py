f985492fdd31b3c0ea65eab165b6712899fbb0d4
unknown <rouchashoori@gmail.com>
2024-08-02 16:00:26 +0330

    some code clean up and fixed some bugs

--- login/views.py
+++ login/views.py
@@ -1,47 +1,85 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from .serializers import UserLoginSerializer, VerifyPhoneNumberSerializer
-from register.models import User
 from rest_framework.response import Response
 from rest_framework import status
-from auth.utils import GenerateError
+from auth.middleware import checkIfUserIsBlocked
+from auth.utils import (
+    generateOTP, 
+    formatPhoneNumber, 
+    checkUserExistance, 
+    getUserIp, 
+    lockoutAccount, 
+    restLockout,
+    generateMessage
+)
+
+BLOCK_MESSAGE = "you've been blocked for 1 hour"
 
 @api_view(["POST"])
+@checkIfUserIsBlocked
+def verifyNumber(request):
+    
+    serializer = VerifyPhoneNumberSerializer(data=request.data)
+    
+    if serializer.is_valid():
+        # get phone number from serializer and replace country code with zero
+        phone_number = serializer.validated_data["phone_number"]
+        phone_number = formatPhoneNumber(phone_number)
+
+        # check if user not exists
+        _, error = checkUserExistance(phone_number)
+
+        if error:
+            # generate a fake otp and pass it to user for just testing
+            code = generateOTP(phone_number)
+            message = generateMessage("'this is temporarily solution for otp' enter this code to continue registering")
+            message["code"] = code
+            return Response(message , status.HTTP_200_OK)
+
+        # if user found
+        message = generateMessage("phone number exists. now login to your account")
+        return Response(message,status.HTTP_200_OK)
+    
+    return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+@checkIfUserIsBlocked
 def loginUser(request):
-    
-    phone_number_header = request.headers.get("phone-number")
+    serializer = UserLoginSerializer(data=request.data)
 
-    if phone_number_header is None:
-        serializer = VerifyPhoneNumberSerializer(data=request.data)
-
-        if serializer.is_valid():
-            
-            phone_number = serializer.validated_data["phone_number"]
-            phone_number = phone_number.replace("+98", "0")
-            try:
-                User.objects.get(phone_number=phone_number)
-
-            except User.DoesNotExist:
-                error = GenerateError("user with this phone number does not exist")
-                return Response(error, status.HTTP_404_NOT_FOUND)
-            
-
-            return Response({"user exists. enter password now"},status.HTTP_200_OK)
+    if serializer.is_valid():
+        # get phone number from serializer and replace country code with zero
+        phone_number = serializer.validated_data["phone_number"]
+        phone_number = formatPhoneNumber(phone_number)
+        userIp       = getUserIp(request)
         
-        return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
-    
-    else:
-        
-        serializer = UserLoginSerializer(data=request.data)
 
-        if serializer.is_valid():
-            phone_number_header = phone_number_header.replace("+98", "0")
-            user = User.objects.get(phone_number=phone_number_header)
-            
-            if user.check_password(serializer.validated_data["password"]):
-                return Response({"detail": "you logged in successfully"} , status.HTTP_200_OK)
-            else:
-                error = GenerateError("wrong phone number or password")
-                return Response(error , status.HTTP_401_UNAUTHORIZED)
-            
-        return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
+        user , error = checkUserExistance(phone_number)
+
+        if error:
+            return error
+        
+        # extracts password from serializer
+        password = serializer.validated_data["password"]
+
+        # check if password is matchs with user password
+        if user.check_password(password):
+            # if password is right so we reset the lockout
+            restLockout(phone_number , userIp)
+            message = generateMessage("you logged in successfully")
+            return Response(message, status.HTTP_200_OK)
+        # if password is wrong one failed attempt will be added to lockout
+        else:
+            error = lockoutAccount(
+                phone_number,
+                userIp, 
+                f"{BLOCK_MESSAGE}, because you've entered wrong phone number or password 3 time."
+            )
+
+            if error:
+                return error
+            message = generateMessage("phone number or password is wrong")
+            return Response(message, status.HTTP_401_UNAUTHORIZED)
+    # bad request errors
+    return Response(serializer.errors , status.HTTP_400_BAD_REQUEST)
