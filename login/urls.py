95b993a358cf4eac60d99789494074ffd2940586
unknown <rouchashoori@gmail.com>
2024-08-02 13:34:05 +0330

    added url for verifying number and user login

--- login/urls.py
+++ login/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("verify/", views.verifyNumber , name="verify-user-number"),
     path("", views.loginUser , name="login-user")
 ]
